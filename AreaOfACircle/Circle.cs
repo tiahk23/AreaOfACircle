5e7499db2695b4a86281e336a1f8b8fd473d283b
Tiah Keefe <tetered12@gmail.com>
2021-07-14 20:33:58 -0500

    Created a new circle class to make code more readible.

---
+++ AreaOfACircle/Circle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaOfACircle
+{
+    public class Circle
+    {
+        public static double GetArea(double r)
+        {
+            double pi = Math.PI;
+            double area = pi * r * r;
+            return area;
+        }
+
+        public static double GetCircumference(double r)
+        {
+            double circumference = 2 * pi * r;
+            return circumference;
+        }
+
+        public static double GetDiameter(double r)
+        {
+
+            double diameter = 2 * r;
+            return diameter;
+        }
+    }
+}
--- AreaOfACircle/Program.cs
+++ AreaOfACircle/Program.cs
@@ -9,16 +9,11 @@ namespace AreaOfACircle
             Console.WriteLine("Enter a radius: ");
             string radiusInput = Console.ReadLine();
             double r = double.Parse(radiusInput);
-            double pi = Math.PI;
-            double area = pi * r * r;
-            Console.WriteLine("The area of a cricle of the radius " + r + " is " + Math.Round(area, 2));
-            double circumference = 2 * pi * r;
-            double diameter = 2 * r;
-            Console.WriteLine("The circumference of the circle is " + Math.Round(circumference, 2) + " and the diamter of the circle is " + Math.Round(diameter, 2));
-
+            Console.WriteLine("The area of a cricle of the radius " + r + " is " + Math.Round(Circle.GetArea(r), 2));
+            Console.WriteLine("The circumference of the circle is " + Math.Round(Circle.GetCircumference(r), 2) + " and the diamter of the circle is " + Math.Round(Circle.GetDiameter(r), 2));
             Console.WriteLine("What are the miles per gallon of your car?");
             string mpg = Console.ReadLine();
-            double gasAmount =  circumference / double.Parse(mpg);
+            double gasAmount =  Circle.GetCircumference(r) / double.Parse(mpg);
             Console.WriteLine("It would require you " +  Math.Round(gasAmount, 2) +" gallons of gas to drive around the circle.");
         }
     }
